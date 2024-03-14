84f20cf152bed7971ffa5236e51de5c742ec6694
Leonardo Sigolo <132359729+LeleoDosCode@users.noreply.github.com>
2024-03-14 14:26:14 -0300

    Add files via upload

---
+++ aulaseis/src/main/java/com/setetres/AulaseisApplication.java
@@ -0,0 +1,13 @@
+package com.setetres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaseisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaseisApplication.class, args);
+	}
+
+}
